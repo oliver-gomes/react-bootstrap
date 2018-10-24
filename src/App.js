b985755d5080fa90e7a634291486f9b8ea3251bb
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-24 01:59:56 -0400

    Navbar import

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowerRouter, Router } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import News from "./components/News";
+import CustomNavbar from "./components/CustomNavbar";
 
 class App extends Component {
   render() {
     return (
-      <BrowerRouter>
+      <BrowserRouter>
         <div>
+          <CustomNavbar />
           <Route path="/" component={Home} exact />
           <Route path="/about" component={About} exact />
           <Route path="/news" component={News} exact />
         </div>
-      </BrowerRouter>
+      </BrowserRouter>
     );
   }
 }
