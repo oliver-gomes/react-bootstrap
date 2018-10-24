1bc52af280d6b33000d0b1f6c1a92082ee5f30fc
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-24 02:07:16 -0400

    update with profile

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,10 +13,19 @@ export default class Home extends Component {
             This is how to build a website with React, React-Router &
             React-Bootstrap
           </p>
+          <Link to="/about">
+            <Button bsStyle="primary">About</Button>
+          </Link>
         </Jumbotron>
-        <Link to="/about">
-          <Button bsStyle="primary">About</Button>
-        </Link>
+        <Row className="show-grid text-center">
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image src="assets/person-1.jpg" circle className="profile-pic" />
+            <p>
+              Take your time. Speed will come later. Decide where your cloud
+              lives. Maybe he lives right in here. There we go{" "}
+            </p>
+          </Col>
+        </Row>
       </Grid>
     );
   }
