96525484c4f39f75412bf92e89cbedd44cfdd2c6
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-24 12:31:10 -0400

    Updates

---
+++ precache-manifest.770156241923c0decd09cfa2444bd778.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93fedfc46a2d0e7c2940",
+    "url": "/react-bootstrap/static/js/runtime~main.93fedfc4.js"
+  },
+  {
+    "revision": "bd40131b220fbbdba496",
+    "url": "/react-bootstrap/static/js/main.bd40131b.chunk.js"
+  },
+  {
+    "revision": "0c2c2c9a7a1ca91a9b1a",
+    "url": "/react-bootstrap/static/js/1.0c2c2c9a.chunk.js"
+  },
+  {
+    "revision": "bd40131b220fbbdba496",
+    "url": "/react-bootstrap/static/css/main.7f51400c.chunk.css"
+  },
+  {
+    "revision": "2dac6ec9b864d9edcb7f0f801f21f6a9",
+    "url": "/react-bootstrap/index.html"
+  }
+];
